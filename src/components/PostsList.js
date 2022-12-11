dc7e83d725f8972d36d7362bba676a4d763c15bf
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-12-10 19:06:08 -0500

    update PostsList

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,24 +1,47 @@
 import React from "react";
 import PostCard from "../components/PostCard";
-import { data } from "../dummy-data/posts";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ALL_POSTS = gql`
+  query getAllPosts {
+    posts {
+      nodes {
+        databaseId
+        title
+        date
+        slug
+        author {
+          node {
+            name
+          }
+        }
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function PostsList() {
-  const loading = false;
-  const error = null;
+ const { loading, error, data } = useQuery(GET_ALL_POSTS);
 
-  if (loading) return <p>Loading posts...</p>;
-  if (error) return <p>Error :(</p>;
+ if (loading) return <p>Loading posts…</p>;
+ if (error) return <p>Error :(</p>;
 
-  const postsFound = Boolean(data?.posts.nodes.length);
-  if (!postsFound) {
-    return <p>No matching posts found.</p>;
-  }
+ const postsFound = Boolean(data?.posts.nodes.length);
+ if (!postsFound) {
+   return <p>No matching posts found.</p>;
+ }
 
-  return (
-    <div className="posts-list">
-      {data.posts.nodes.map((post) => (
-        <PostCard key={post.databaseId} post={post} />
-      ))}
-    </div>
-  );
+ return (
+   <div className="posts-list">
+     {data.posts.nodes.map((post) => (
+       <PostCard key={post.databaseId} post={post} />
+     ))}
+   </div>
+ );
 }
